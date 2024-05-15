777b1e1ffe684524dd9b15f5ef4b553f757fda81
halilbeytorun <beytorun25@gmail.com>
2024-05-16 01:23:16 +0300

    handle socket closing scenerio for LoggingHandler.
    Since the socket can be closed both from server side or client side,
    server side should handle if it is closed from client side. It should
    close the clientSocket on the server side if the Client already closed
    it

--- src/LoggingHandler.cpp
+++ src/LoggingHandler.cpp
@@ -5,24 +5,36 @@
 #include <cstdio>
 #include <iostream>
 
-LoggingHandler::LoggingHandler(int _clientSocket) : clientSocket(_clientSocket)
+LoggingHandler::LoggingHandler(int _clientSocket) : m_clientSocket(_clientSocket), m_socketClosed(false)
 {
 
+}
+LoggingHandler::~LoggingHandler()
+{
+    if(!m_socketClosed)
+        close(m_clientSocket);
 }
 
 int LoggingHandler::get_handle()
 {
-    return clientSocket;
+    return m_clientSocket;
 }
 
 
 int LoggingHandler::handle_event(EventType)
 {
         // Read data from the client
-    int bytesReceived = read(clientSocket, buffer, sizeof(buffer));
+    int bytesReceived = read(m_clientSocket, buffer, sizeof(buffer));
     if (bytesReceived < 0) {
         perror("Read failed");
         return -1;
+    }
+    else if(0 == bytesReceived) // Connection closed --> Action needed.
+    {
+        close(m_clientSocket);
+        m_socketClosed = true;
+        Logger("LoggingHandler::handle_event client socket is closed from server side!");
+        return 0;
     }
     buffer[bytesReceived] = '\0';
     Logger("LoggingHandler::handle_event", std::string{buffer}, " message is read");
--- src/LoggingHandler.h
+++ src/LoggingHandler.h
@@ -8,12 +8,14 @@ class LoggingHandler : public EventHandler
 {
 public:
     explicit LoggingHandler(int _clientSocket);
+    ~LoggingHandler();
     int handle_event(EventType) override;
     int get_handle() override;
 
 private:
-    int clientSocket;
-    char buffer[1024];
+    int m_clientSocket{};
+    char buffer[1024]{};
+    bool m_socketClosed{};
 };
 
 
